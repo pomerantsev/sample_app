c1440cb0dd67fd0cffef9cd2878565664f72f13f
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-11-25 16:51:42 +0400

    (lesson 5) use named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root "static_pages#home"
+  get "/about" => "static_pages#about"
+  get "/help" => "static_pages#help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
