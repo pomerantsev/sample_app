4292578251d760e947a11c0690bc01dca1615a83
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-11-25 16:50:41 +0400

    Modify full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def full_title(base_title)
-    "Ruby on Rails Tutorial Sample App | #{base_title}"
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.blank?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
   end
 end
