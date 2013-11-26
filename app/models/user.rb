967253261463654fb877e65ada0988140c68d31c
Pavel Pomerantsev <pomerantsevp@gmail.com>
2013-11-26 13:03:47 +0400

    (lesson 8) implement signing in

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,24 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
+  before_create :create_remember_token
   before_save :convert_email_to_downcase
 
   has_secure_password
 
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
   private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 
   def convert_email_to_downcase
     email.downcase!
